2d0f8014a2be78ec824a0a8a4506bed83037b9c1
Nikolay Prieto <nikolay@mvnifest.com>
2024-02-24 21:44:55 -0500

    ommiting the try to know the error

--- JetsonNano/start_automatic_gait.py
+++ JetsonNano/start_automatic_gait.py
@@ -128,19 +128,19 @@ def main(id, command_status):
 
 
 if __name__ == "__main__":
-    try:
-        # Keyboard input Process
-        KeyInputs = KeyInterrupt()
-        KeyProcess = Process(target=KeyInputs.keyInterrupt, args=(1, KeyInputs.key_status, KeyInputs.command_status))
-        KeyProcess.start()
+    # try:
+    # Keyboard input Process
+    KeyInputs = KeyInterrupt()
+    KeyProcess = Process(target=KeyInputs.keyInterrupt, args=(1, KeyInputs.key_status, KeyInputs.command_status))
+    KeyProcess.start()
 
-        # Main Process 
-        main(2, KeyInputs.command_status)
-        
-        print("Terminate KeyBoard Input process")
-        if KeyProcess.is_alive():
-            KeyProcess.terminate()
-    except Exception as e:
-        print(e)
-    finally:
-        print("Done... :)")
+    # Main Process 
+    main(2, KeyInputs.command_status)
+    
+    print("Terminate KeyBoard Input process")
+    if KeyProcess.is_alive():
+        KeyProcess.terminate()
+    # except Exception as e:
+    #     print(e)
+    # finally:
+    #     print("Done... :)")
